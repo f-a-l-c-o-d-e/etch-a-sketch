0480860999b4c1d05c9bad35aa71e5a75d14d754
f-a-l-c-o-d-e <178407688+f-a-l-c-o-d-e@users.noreply.github.com>
2024-09-10 10:33:10 +0200

    feat: Add the paint square functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const grid = document.querySelector(".grid")
+function createGrid (numberOfDivs) {
+    const grid = document.querySelector(".grid")
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div")
-    div.classList.add("grid-element")
-    grid.appendChild(div)
+    for (let i = 0; i < numberOfDivs; i++) {
+        const div = document.createElement("div")
+        div.classList.add("grid-element")
+        grid.appendChild(div)
+    }
 }
+
+function paintGrid () {
+    const gridElements = document.querySelectorAll(".grid-element")
+
+    gridElements.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red"
+            setTimeout(() => {
+                div.style.backgroundColor = "white"
+            }, 500)
+        })
+    })
+}
+
+createGrid(16*16)
+paintGrid()
