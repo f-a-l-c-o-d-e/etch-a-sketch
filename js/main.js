4343c5d4103630c1b85533ac03f93c3b3497fb84
f-a-l-c-o-d-e <178407688+f-a-l-c-o-d-e@users.noreply.github.com>
2024-09-10 11:50:14 +0200

    feat: Add the button to change dimension

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
-function createGrid (numberOfDivs) {
-    const grid = document.querySelector(".grid")
+function createGrid (numberOfDivsForSide=16) {
+    function showGrid (numberOfDivsForSide) {
+        const grid = document.querySelector(".grid")
+        const numberOfDivs = numberOfDivsForSide ** 2
+        for (let i = 0; i < numberOfDivs; i++) {
+            const div = document.createElement("div")
+            div.classList.add("grid-element")
+            div.style.padding = `${(640-(2*numberOfDivsForSide))/(2*numberOfDivsForSide)}px`
+            grid.appendChild(div)
+        }
+    }
+    
+    function paintGrid () {
+        const gridElements = document.querySelectorAll(".grid-element")
+    
+        gridElements.forEach((div) => {
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "red"
+                setTimeout(() => {
+                    div.style.backgroundColor = "white"
+                }, 500)
+            })
+        })
+    }
+    
+    showGrid(numberOfDivsForSide)
+    paintGrid()
+}
 
-    for (let i = 0; i < numberOfDivs; i++) {
-        const div = document.createElement("div")
-        div.classList.add("grid-element")
-        grid.appendChild(div)
+function changeGrid () {
+    const input = prompt("Insert the number of squares per side (max 100):")
+    if (input > 100 || Number(input) === NaN || input === "") {
+        changeGrid()
+    } else {
+        const grid = document.querySelector(".grid")
+        const gridElements = document.querySelectorAll(".grid-element")
+        gridElements.forEach(div => {
+            grid.removeChild(div)
+        })
+        createGrid(Number(input))
     }
 }
 
-function paintGrid () {
-    const gridElements = document.querySelectorAll(".grid-element")
+const button = document.querySelector(".gridButton")
 
-    gridElements.forEach((div) => {
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "red"
-            setTimeout(() => {
-                div.style.backgroundColor = "white"
-            }, 500)
-        })
-    })
-}
 
-createGrid(16*16)
-paintGrid()
+
+
+createGrid(16)
+
+button.addEventListener("click", () => changeGrid())
